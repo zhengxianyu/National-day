3e5dc09fbe940963ad3f166b974c03eba648f60a
vivien <1456826641@qq.com>
2019-09-27 13:11:27 +0800

    update house

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 $(function() {
+  function numberRandom(max, min) {
+    var num = ( Math.random() * ( max - min ) + min ).toFixed( 2 )
+    return num;
+  }
+
   createRedFlag();
+  createInsideWall();
 
   // 红旗
   function createRedFlag() {
@@ -13,6 +19,22 @@ $(function() {
         (16 - 0.1 * index) +
           's);';
       document.getElementById('redFlag').appendChild(redLine);
+    }
+  }
+
+  // 内墙
+  function createInsideWall() {
+    for (let index = 0; index < 5; index ++) {
+      let wallBlock = document.createElement('div');
+      wallBlock.className = 'wall-block';
+      wallBlock.style = 'left: ' + 1.3 * index + 'vw;';
+
+      let wallColumn = document.createElement('div');
+      wallColumn.className = 'wall-column';
+      wallColumn.style = 'left: ' + (1 + 1.3 * index) + 'vw;';
+
+      document.getElementById('rightInsideWall').appendChild(wallBlock);
+      document.getElementById('rightInsideWall').appendChild(wallColumn);
     }
   }
 })
